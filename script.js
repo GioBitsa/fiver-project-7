8b49f4551db2e8a2cf3bdf1d352feb541df792bb
giobitsa <giorgi.bitsadze01@gmail.com>
2022-04-13 13:28:56 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const sideBar = document.getElementById('sideBar')
+const navClose = document.getElementById('navClose')
+const mobileNav = document.getElementById('mobileNav')
+const headerContainer = document.getElementById('headerContainer')
+const searchBtn = document.getElementById('searchBtn')
+const close = document.getElementById('close')
+const searchContainer = document.getElementById('searchContainer')
+
+searchBtn.addEventListener('click', () => {
+    searchContainer.classList.add('visible')
+})
+
+close.addEventListener('click', () => {
+    searchContainer.classList.remove('visible')
+})
+
+headerContainer.addEventListener('mouseover', () => {
+    headerContainer.classList.add('hover')
+})
+
+headerContainer.addEventListener('mouseleave', () => {
+    if (window.scrollY < 100 && mobileNav.classList.contains(!'active')) {
+        headerContainer.classList.remove('hover')
+    }
+})
+
+document.addEventListener('scroll', () => {
+    if (window.scrollY > 1000) {
+        headerContainer.classList.add('fixed')
+        headerContainer.classList.add('hover')
+    } else if (window.scrollY < 100) {
+        headerContainer.classList.remove('fixed')
+        headerContainer.classList.remove('hover')
+    }
+})
+
+sideBar.addEventListener('click', () => {
+    mobileNav.classList.toggle('active')
+    sideBar.classList.toggle('navOpened')
+    navClose.classList.toggle('navOpened')
+})

eee9af7af5292023fe65c05a02eb52fdb1690e59
giobitsa <giorgi.bitsadze01@gmail.com>
2022-04-13 15:56:56 +0400

    added comments

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+
+// burger icon
 const sideBar = document.getElementById('sideBar')
+
+// mobile navigation menu container
 const mobileNav = document.getElementById('mobileNav')
+
+// header container
 const headerContainer = document.getElementById('headerContainer')
+
+// search button, which is for openning search container
 const searchBtn = document.getElementById('searchBtn')
+
+// close button on search container
 const close = document.getElementById('close')
+
+// search container
 const searchContainer = document.getElementById('searchContainer')
 
+
+// search container open/close
 searchBtn.addEventListener('click', () => {
     searchContainer.classList.add('visible')
 })
@@ -13,6 +27,7 @@ close.addEventListener('click', () => {
     searchContainer.classList.remove('visible')
 })
 
+// animation when you hover on header
 headerContainer.addEventListener('mouseover', () => {
     headerContainer.classList.add('hover')
 })
@@ -23,7 +38,8 @@ headerContainer.addEventListener('mouseleave', () => {
     }
 })
 
-let lastScrollNumber = 0;
+// animation when you scroll
+let lastScrollNumber = 0; // number which is for detecting scrolling direction
 
 document.addEventListener('scroll', () => {
 
@@ -52,11 +68,13 @@ document.addEventListener('scroll', () => {
     }
 })
 
+// burger icon functionality
 sideBar.addEventListener('click', () => {
     mobileNav.classList.toggle('active')
     sideBar.classList.toggle('animate')
 })
 
+// if you open mobile navigation menu and then resize window, that navigation should be closed
 window.addEventListener('resize', () => {
     if (window.innerWidth > 768 && mobileNav.classList.contains('active')) {
         mobileNav.classList.remove('active')
